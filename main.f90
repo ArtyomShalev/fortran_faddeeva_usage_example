5c23caaa35f554c9141688bd40f84f86ac1e4b00
ArtyomShalev <artyomshalev@gmail.com>
2022-02-17 17:06:45 +0300

    changed usage example

--- main.f90
+++ main.f90
@@ -1,27 +1,19 @@
 program fortran_faddeeva_usage_example
     use faddeeva_fortran_interface
+    !-----------------------------------------
     implicit none
-
     complex(dp) :: z
-    real(dp) :: relerr
-    character(len=16) :: func_name
-
-    interface
-        function func(z, relerr)
-            use iso_c_binding
-            complex(c_double_complex), value :: z
-            real(c_double), value :: relerr
-            complex(c_double_complex) :: func
-        end function
-    end interface
-
-    procedure (func), pointer :: f_ptr => null()
-
-    z = cmplx(1.0_dp, 1.0_dp, dp)
-    relerr = 0.0_dp
-    func_name = 'Faddeeva_erf'
-    f_ptr => Faddeeva_erf
-
-    call example(f_ptr, func_name, z, relerr)
-
+    real(dp) :: relerr, relerr_to_print
+    !-----------------------------------------
+    z = cmplx(6.0_dp, 0.158489319246111_dp, dp)
+    relerr = 0.0_dp !machine precision, see Faddeeva.cc in original MIT Faddeeva package
+    if (relerr < 2.22e-16_dp) relerr_to_print = 2.22e-16_dp !according to https://en.wikipedia.org/wiki/Machine_epsilon
+    print *, 'We demand relative error =', relerr_to_print
+    print *, 'z = ', z
+    print *, 'w(z) =' , Faddeeva_w(z, relerr)
+    print *, 'erf(z) =' , Faddeeva_erf(z, relerr)
+    print *, 'erfc(z) =', Faddeeva_erfc(z, relerr)
+    print *, 'erfcx(z) =', Faddeeva_erfcx(z, relerr)
+    print *, 'erfi(z) =', Faddeeva_erfi(z, relerr)
+    print *, 'Dawson(z) =', Faddeeva_dawson(z, relerr)
 end program
